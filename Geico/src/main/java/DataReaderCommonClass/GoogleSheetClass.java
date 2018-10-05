5da81f84a296496caa29c5f5a98bace1f3236330
afia <afiafarjana560@gmail.com>
2018-10-05 01:05:29 -0400

    new again

--- Geico/src/main/java/DataReaderCommonClass/GoogleSheetClass.java
+++ Geico/src/main/java/DataReaderCommonClass/GoogleSheetClass.java
@@ -18,7 +18,6 @@ import static base.GoogleSheetReader.getSheetsService;
 public class GoogleSheetClass extends Login {
     Login loginObject = PageFactory.initElements(driver,Login.class);
 
-
     public List<List<Object>> getSpreadSheetRecords(String spreadsheetId, String range) throws IOException {
         // Build a new authorized API client service.
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
@@ -31,47 +30,18 @@ public class GoogleSheetClass extends Login {
             return null;
         } else {
             return values;
+
         }
     }
-    public List<String> signInByInvalidIdPass(String spreadsheetId, String range) throws IOException, InterruptedException {
+    public void signInByInvalidIdPass(String spreadsheetId, String range) throws IOException, InterruptedException {
         TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        navigateToLoginPage();
         List<List<Object>> col2Value = getSpreadSheetRecords(spreadsheetId, range);
-        List<String> actual = new ArrayList<>();
         for (List row : col2Value) {
-            Thread.sleep(3000);
-            inputValueInTextBoxByWebElement(loginTextBox, row.get(1).toString());
-            inputValueInTextBoxByWebElement(passwordTextBox, row.get(2).toString());
-            Thread.sleep(3000);
-            actual.add(getTextByWebElement(errorMessage));
-            System.out.println(getTextByWebElement(errorMessage));
-            clearInputBox(loginTextBox);
-            clearInputBox(passwordTextBox) ;
-            Thread.sleep(3000);
+           String expectedMessage = SendLoginInfo(row.get(0).toString(),row.get(1).toString());
+           String actualMessage = row.get(2).toString();
+           Assert.assertEquals(actualMessage,expectedMessage);
         }
-        return actual;
-    }
-    public void inputValueInTextBoxByWebElement(WebElement webElement, String value){
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        webElement.sendKeys(value + Keys.ENTER);
-    }
-    public void clearInputBox(WebElement webElement){
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        webElement.clear();
-    }
-    public String getTextByWebElement(WebElement webElement){
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        String text = webElement.getText();
-        return text;
-    }
-    public void sendSpreadSheetIdAndRange(String spreadsheetId, String range) throws InterruptedException, IOException {
-        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
-        driver.navigate().to("https://ecams.geico.com/ecams/login.xhtml?r=true");
-        Thread.sleep(1000);
-        List<String> actualErrorMessage = signInByInvalidIdPass(spreadsheetId, range);
-        loginObject.clickSubmit();
-        List<List<Object>> expectedErrorMessage = getSpreadSheetRecords(spreadsheetId, range);
-        for (int index=0;index<expectedErrorMessage.size();index++) {
-            Assert.assertTrue(actualErrorMessage.get(index).contains(actualErrorMessage.get(index)));
-        }
+
     }
 }
